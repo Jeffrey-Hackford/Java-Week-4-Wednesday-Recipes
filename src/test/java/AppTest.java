79af3a8b6c190accad4daba08e7c3e5de61ccbe0
Jacob Hefley and Jacob Hefley <jacob@>
2016-05-11 09:08:14 -0700

    add initial files structure

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,28 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.sql2o.*;
+import org.junit.*;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Recipe Book");
+  }
+}
